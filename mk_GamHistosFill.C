bfbf9b737410663843c960ac1d1a30e595c17a96
blehtela <142437787+blehtela@users.noreply.github.com>
2024-02-22 17:51:20 +0200

    Test version for trying to figure out the difference between plots with 2022-jec and 2023-jec

--- mk_GamHistosFill.C
+++ mk_GamHistosFill.C
@@ -46,7 +46,9 @@ R__LOAD_LIBRARY(GamHistosFill_C.so)
 #endif
 
 
-void mk_GamHistosFill(string dataset = "X", string version = "w5") { //using w-version names for my code (Bettina).
+//void mk_GamHistosFill(string dataset = "X", string version = "w5") { //using w-version names for my code (Bettina).
+void mk_GamHistosFill(string dataset = "X", string version = "wX23") { //using version wX23 for summer23 corrections TEST (with one single file per iov)
+
 
   // Settings
   bool addData = (dataset=="2016B"  || dataset=="2016C" || dataset=="2016D" || 
